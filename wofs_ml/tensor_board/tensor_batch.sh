338c7ab09998e830d76527b0819bc6153415d238
Chad Wiley <chadwiley@schooner1.oscer.ou.edu>
2023-01-31 20:40:44 -0600

    new gitignore

--- wofs_ml/tensor_board/tensor_batch.sh
+++ wofs_ml/tensor_board/tensor_batch.sh
@@ -4,11 +4,11 @@
 #SBATCH --ntasks=1
 #SBATCH --mem=4G
 #SBATCH --time=00:30:00
-#SBATCH --job-name=board
+#SBATCH --job-name=tensorboard
 #SBATCH --mail-user=chadwiley@ou.edu
 #SBATCH --mail-type=ALL
-#SBATCH --output=/home/chadwiley/research/wofs_ml_ci/wofs_ml/slurmouts/R-%x.%j.out
-#SBATCH --error=/home/chadwiley/research/wofs_ml_ci/wofs_ml/slurmouts/R-%x.%j.err
+#SBATCH --output=/home/chadwiley/research/wofs_40dbz/wofs_ml/slurmouts/R-%x.%j.out
+#SBATCH --error=/home/chadwiley/research/wofs_40dbz/wofs_ml/slurmouts/R-%x.%j.err
 
 #source you python env
 source /home/chadwiley/.bashrc
@@ -16,4 +16,4 @@ bash
 
 conda activate tf
 
-tensorboard --logdir="/ourdisk/hpc/ai2es/chadwiley/boardlogs/log1/" --port=6098 --bind_all
+tensorboard --logdir="/ourdisk/hpc/ai2es/chadwiley/boardlogs/log1/" --port=6031 --bind_all
